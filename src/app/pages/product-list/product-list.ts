95ac48355b849f2bb90068754e755c84805294c7
EricWithC04 <negativegame117@gmail.com>
2026-06-18 10:41:00 -0300

    feat: product list connected with the backend

--- src/app/pages/product-list/product-list.ts
+++ src/app/pages/product-list/product-list.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { Sidebar } from '../../components/sidebar/sidebar';
 import { ProductCard } from '../../components/product-card/product-card';
+import { ProductService } from '../../services/product';
 
 @Component({
   selector: 'app-product-list',
@@ -9,6 +10,21 @@ import { ProductCard } from '../../components/product-card/product-card';
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
-export class ProductList {
-  products: any[] = [1, 2, 3, 4];
+export class ProductList implements OnInit {
+  products: any[] = [];
+
+  constructor(
+    private productService: ProductService,
+    private cdf: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    
+    this.productService.getProducts()
+      .subscribe(products => {
+        this.products = products
+        this.cdf.detectChanges()
+      })
+
+  }
 }
--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -4,7 +4,16 @@ import { Observable } from 'rxjs';
 import { Service } from '@angular/core'
 import { Product } from '../models/product.model';
 
-@Service()
+@Injectable({
+    providedIn: 'root'
+})
 export class ProductService {
     
+    private apiUrl = 'http://localhost:8080/productos';
+
+    constructor(private http: HttpClient) { }
+
+    getProducts(): Observable<any[]> {
+        return this.http.get<any[]>(this.apiUrl);
+    } 
 }
