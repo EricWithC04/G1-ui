684080c5785074df2bbe205a2b6cc9599c96cd21
EricWithC04 <negativegame117@gmail.com>
2026-06-18 12:16:45 -0300

    feat: button to redirect to new-product page

---
+++ src/app/pages/create-product/create-product.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
+
+@Component({
+  selector: 'app-create-product',
+  imports: [Header],
+  templateUrl: './create-product.html',
+  styleUrl: './create-product.css',
+})
+export class CreateProduct {}
