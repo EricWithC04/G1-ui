a1579b90debda919c04e0f3a2ad1618d54a3220b
EricWithC04 <negativegame117@gmail.com>
2026-06-18 12:56:04 -0300

    feat: button to back to main page in new-product

--- src/app/pages/create-product/create-product.ts
+++ src/app/pages/create-product/create-product.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
+import { LucideArrowLeft } from '@lucide/angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-product',
-  imports: [Header],
+  imports: [Header, LucideArrowLeft, RouterLink],
   templateUrl: './create-product.html',
   styleUrl: './create-product.css',
 })
