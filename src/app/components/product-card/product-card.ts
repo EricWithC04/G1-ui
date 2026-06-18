015eeb4a1a2d96f776f75648aeb307148fde7647
EricWithC04 <negativegame117@gmail.com>
2026-06-18 09:59:10 -0300

    feat: basic component of card

---
+++ src/app/components/product-card/product-card.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+  host: {
+    class: "flex flex-col border-white/20 border-1 min-h-90 rounded-lg overflow-hidden"
+  }
+})
+export class ProductCard {}
--- src/app/pages/product-list/product-list.ts
+++ src/app/pages/product-list/product-list.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { Sidebar } from '../../components/sidebar/sidebar';
+import { ProductCard } from '../../components/product-card/product-card';
 
 @Component({
   selector: 'app-product-list',
-  imports: [Header, Sidebar],
+  imports: [Header, Sidebar, ProductCard],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
-export class ProductList {}
+export class ProductList {
+  products: any[] = [1, 2, 3, 4];
+}
