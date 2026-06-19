a3e37d35bf42dbf2c065fa6c738610f738c66f5a
EricWithC04 <negativegame117@gmail.com>
2026-06-19 01:08:21 -0300

    fix: error of compile for unused model

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Service } from '@angular/core'
-import { Product } from '../models/product.model';
+// import { Product } from '../models/product.model';
 
 @Injectable({
     providedIn: 'root'
